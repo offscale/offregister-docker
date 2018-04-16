ee7c84c83d8f7fc36c2ad453bacaa528d48b8890
Samuel Marks <samuelmarks@gmail.com>
2018-04-17 03:08:15 +1000

    Init

---
+++ README.md
@@ -0,0 +1,20 @@
+offregister_docker
+==================
+This package follows the offregister library specification for Docker.
+
+
+## Install package
+
+    pip install .
+
+## Example use in offregister JSON
+
+    {
+        "module": "offregister-docker",
+        "kwargs": {
+        }
+    }
+
+---
+
+To setup your environment to use this config, follow [the getting started guide](https://offscale.io/docs/getting-started).
---
+++ offregister_docker/centos.py
@@ -0,0 +1,47 @@
+from functools import partial
+from itertools import imap
+from os import path
+from sys import modules
+from pkg_resources import resource_filename
+
+from fabric.contrib.files import append
+from fabric.operations import sudo
+
+from offregister_fab_utils.fs import cmd_avail
+
+
+def install_docker0(*args, **kwargs):
+    if cmd_avail('docker'):
+        return
+
+    kernel_version = sudo('uname -r', quiet=True)
+    if tuple(imap(int, kernel_version[:kernel_version.find('.', 2)].split('.'))) < (3, 10):
+        raise NotImplementedError('Docker for older versions of the Linux kernel')
+
+    for mnt in sudo('mount | grep "^/dev"'):
+        if 'xfs' not in mnt and len(mnt) > 1:
+            print 'mnt =', mnt
+            dev = mnt[:mnt.find(' on')]
+            print 'dev =', dev
+            sudo('mkfs -t xfs -n ftype=1 {dev}'.format(dev=dev))
+
+    if sudo('lsmod | grep overlay', warn_only=True).failed:
+        append('/etc/modules-load.d/overlay.conf', 'overlay', use_sudo=True)
+        sudo('reboot')
+        return 'rebooting: RERUN'
+
+    centos_join = partial(path.join,
+                          path.join(path.dirname(
+                              resource_filename(modules[__name__].__package__, '__init__.py')), '_centos'))
+    with open(centos_join('docker.yum')) as f:
+        append('/etc/yum.repos.d/docker.repo', f.read(), use_sudo=True)
+
+    with open(centos_join('override.conf')) as f:
+        p = '/etc/systemd/system/docker.service.d'
+        sudo('mkdir -p {p}'.format(p=p))
+        append('{p}/override.conf'.format(p=p), f.read(), use_sudo=True)
+    sudo('yum install -y docker-engine-1.13.1 docker-engine-selinux-1.13.1')
+    sudo('systemctl start docker')
+    sudo('systemctl enable docker')
+
+    return sudo('docker ps')
