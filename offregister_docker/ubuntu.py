deb12c785b1e4881722a1c0dfb946e7ee60a5a03
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_docker/ubuntu.py
+++ offregister_docker/ubuntu.py
@@ -5,45 +5,59 @@ from offregister_fab_utils.fs import cmd_avail
 
 
 def install_docker0(*args, **kwargs):
-    if cmd_avail('docker'):
-        return 'Docker is already installed'
+    if cmd_avail("docker"):
+        return "Docker is already installed"
 
-    apt_depends('apt-transport-https', 'ca-certificates', 'curl', 'gnupg-agent', 'software-properties-common')
-    sudo('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -')
-    sudo('apt-key fingerprint 0EBFCD88')
-    distro = run('lsb_release -cs')
-    sudo('add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu {distro} stable"'.format(
-        distro=distro
-    ))
-    apt_depends('docker-ce', 'docker-ce-cli', 'containerd.io')
-    sudo('systemctl enable docker')
-    return 'Docker is now installed and will run at boot'
+    apt_depends(
+        "apt-transport-https",
+        "ca-certificates",
+        "curl",
+        "gnupg-agent",
+        "software-properties-common",
+    )
+    sudo("curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -")
+    sudo("apt-key fingerprint 0EBFCD88")
+    distro = run("lsb_release -cs")
+    sudo(
+        'add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu {distro} stable"'.format(
+            distro=distro
+        )
+    )
+    apt_depends("docker-ce", "docker-ce-cli", "containerd.io")
+    sudo("systemctl enable docker")
+    return "Docker is now installed and will run at boot"
 
 
 def install_docker_user1(*args, **kwargs):
-    user = run('echo $USER', quiet=True)
-    if run('getent group docker', warn_only=True).failed:
-        sudo('groupadd docker')
-    elif run('id -nG "{user}" | grep -qw docker'.format(user=user), warn_only=True, quiet=True).failed:
-        sudo('usermod -aG docker {user}'.format(user=user))
+    user = run("echo $USER", quiet=True)
+    if run("getent group docker", warn_only=True).failed:
+        sudo("groupadd docker")
+    elif run(
+        'id -nG "{user}" | grep -qw docker'.format(user=user),
+        warn_only=True,
+        quiet=True,
+    ).failed:
+        sudo("usermod -aG docker {user}".format(user=user))
     else:
-        return 'Docker group already exists'
+        return "Docker group already exists"
 
-    raise NotImplementedError('You must restart machine for nonroot user to run Docker')
+    raise NotImplementedError("You must restart machine for nonroot user to run Docker")
 
 
 def test_docker2(*args, **kwargs):
-    return run('docker run hello-world')
+    return run("docker run hello-world")
 
 
 def install_docker_compose3(*args, **kwargs):
-    version = '1.25.4'
+    version = "1.25.4"
 
-    if cmd_avail('docker-compose'):
-        return 'already installed'
+    if cmd_avail("docker-compose"):
+        return "already installed"
 
-    executable = '/usr/local/bin/docker-compose'
+    executable = "/usr/local/bin/docker-compose"
 
-    sudo('curl -L https://github.com/docker/compose/releases/download/{version}/docker-compose-$(uname -s)-$(uname -m)'
-         ' -o {executable}'.format(executable=executable, version=version))
-    sudo('chmod +x {executable}'.format(executable=executable))
+    sudo(
+        "curl -L https://github.com/docker/compose/releases/download/{version}/docker-compose-$(uname -s)-$(uname -m)"
+        " -o {executable}".format(executable=executable, version=version)
+    )
+    sudo("chmod +x {executable}".format(executable=executable))
